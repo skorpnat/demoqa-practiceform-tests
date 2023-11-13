a105135835f8d38a31cb825e0ecd6fa57db115ff
nklimova <NKlimova@it-one.ru>
2023-11-13 11:16:57 +0600

    add generation data using faker lib

---
+++ src/test/java/data/TestData.java
@@ -0,0 +1,39 @@
+package data;
+
+import com.github.javafaker.Faker;
+import java.util.Locale;
+
+public class TestData {
+    Faker faker = new Faker(new Locale("en-GB"));
+    public String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            gender = faker.options().option("Male", "Female", "Other"),
+            phoneNumber = faker.phoneNumber().subscriberNumber(10),
+            invalidPhoneNumber = faker.phoneNumber().subscriberNumber(4),
+            day = String.valueOf(faker.number().numberBetween(1, 28)),
+            month = faker.options().option("January", "February", "March", "April", "May", "June", "July",
+                    "August", "September", "October", "November", "December"),
+            year = String.valueOf(faker.number().numberBetween(1990, 2000)),
+            subjects = faker.options().option("Social Studies", "Arts", "Chemistry"),
+            hobbies = faker.options().option("Sports", "Reading", "Music"),
+            picture = faker.options().option("badger.jpg", "cat.jpg", "dog.jpg"),
+            currentAddress = faker.address().streetAddress(),
+            state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan"),
+            city = setCity(state);
+
+    public String setCity(String state) {
+        switch (state) {
+            case "NCR":
+                return faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh":
+                return faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana":
+                return faker.options().option("Karnal", "Panipat");
+            case "Rajasthan":
+                return faker.options().option("Jaipur", "Jaiselmer");
+            default:
+                return null;
+        }
+    }
+}
--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -1,54 +1,56 @@
 package tests;
 
+import data.TestData;
 import org.junit.jupiter.api.Test;
 import pages.PracticeFormPage;
 
 public class PracticeFormTest extends TestBase {
     PracticeFormPage practiceFormPage = new PracticeFormPage();
-
+    TestData testData = new TestData();
     @Test
     void fillFormTest() {
+
         practiceFormPage.openPage()
                 .cleanAdvertisementOnPage()
-                .setFirstName("Nata")
-                .setLastName("Natanova")
-                .setGender("Female")
-                .setPhoneNumber("9136754576")
-                .setDateOfBirth("30", "December", "1974")
-                .setUserEmail("nnatta@test.com")
-                .setHobbies("Reading")
-                .setSubject("Math")
-                .setCurrentAddress("CurrentAddress str 234")
-                .uploadFile("cat.png")
-                .setState("Uttar Pradesh")
-                .setCity("Lucknow")
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setGender(testData.gender)
+                .setPhoneNumber(testData.phoneNumber)
+                .setDateOfBirth(testData.day, testData.month, testData.year)
+                .setUserEmail(testData.userEmail)
+                .setHobbies(testData.hobbies)
+                .setSubject(testData.subjects)
+                .setCurrentAddress(testData.currentAddress)
+                .uploadFile(testData.picture)
+                .setState(testData.state)
+                .setCity(testData.city)
                 .submit();
 
-        practiceFormPage.checkResultTable("Student Name", "Nata Natanova")
-                .checkResultTable("Student Email", "nnatta@test.com")
-                .checkResultTable("Gender", "Female")
-                .checkResultTable("Mobile", "9136754576")
-                .checkResultTable("Date of Birth", "30 December,1974")
-                .checkResultTable("Subjects", "Maths")
-                .checkResultTable("Hobbies", "Reading")
-                .checkResultTable("Picture", "cat.png")
-                .checkResultTable("Address", "CurrentAddress str 234")
-                .checkResultTable("State and City", "Uttar Pradesh Lucknow");
+        practiceFormPage.checkResultTable("Student Name", testData.firstName + " " + testData.lastName)
+                .checkResultTable("Student Email", testData.userEmail)
+                .checkResultTable("Gender", testData.gender)
+                .checkResultTable("Mobile", testData.phoneNumber)
+                .checkResultTable("Date of Birth", testData.day + " " + testData.month + "," + testData.year)
+                .checkResultTable("Subjects", testData.subjects)
+                .checkResultTable("Hobbies", testData.hobbies)
+                .checkResultTable("Picture", testData.picture)
+                .checkResultTable("Address", testData.currentAddress)
+                .checkResultTable("State and City", testData.state + " " + testData.city);
     }
 
     @Test
     void inputMinimalData() {
         practiceFormPage.openPage()
                 .cleanAdvertisementOnPage()
-                .setFirstName("Nata")
-                .setLastName("Natanova")
-                .setGender("Female")
-                .setPhoneNumber("9136754576")
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setGender(testData.gender)
+                .setPhoneNumber(testData.phoneNumber)
                 .submit();
 
-        practiceFormPage.checkResultTable("Student Name", "Nata Natanova")
-                .checkResultTable("Gender", "Female")
-                .checkResultTable("Mobile", "9136754576");
+        practiceFormPage.checkResultTable("Student Name", testData.firstName + " " + testData.lastName)
+                .checkResultTable("Gender", testData.gender)
+                .checkResultTable("Mobile", testData.phoneNumber);
 
     }
 
@@ -56,10 +58,10 @@ public class PracticeFormTest extends TestBase {
     void inputInvalidMobileNumber() {
         practiceFormPage.openPage()
                 .cleanAdvertisementOnPage()
-                .setFirstName("Nata")
-                .setLastName("Natanova")
-                .setGender("Female")
-                .setPhoneNumber("4576")
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setGender(testData.gender)
+                .setPhoneNumber(testData.invalidPhoneNumber)
                 .submit();
 
         practiceFormPage.verifyMobileNumberFieldIsRed();
