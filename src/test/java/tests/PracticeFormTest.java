53743178b6d450db7425daab44ee49387ccc3f8c
Klimova Natalia <skorpnat@gmail.com>
2023-12-18 12:21:43 +0600

    add jenkins

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -1,18 +1,27 @@
 package tests;
 
 import data.TestData;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.PracticeFormPage;
+import org.junit.jupiter.api.Tag;
+import static io.qameta.allure.Allure.step;
 
+@Tag("demoqa")
 public class PracticeFormTest extends TestBase {
     PracticeFormPage practiceFormPage = new PracticeFormPage();
     TestData testData = new TestData();
-    @Test
-    void fillFormTest() {
 
-        practiceFormPage.openPage()
-                .cleanAdvertisementOnPage()
-                .setFirstName(testData.firstName)
+    @Test
+    @DisplayName("Проверка формы со всеми заполненными полями")
+    void fillFormTest() {
+        step("Открыть форму", () -> {
+            practiceFormPage.openPage()
+                    .cleanAdvertisementOnPage();
+        });
+
+        step("Ввести данные", () -> {
+        practiceFormPage.setFirstName(testData.firstName)
                 .setLastName(testData.lastName)
                 .setGender(testData.gender)
                 .setPhoneNumber(testData.phoneNumber)
@@ -25,7 +34,9 @@ public class PracticeFormTest extends TestBase {
                 .setState(testData.state)
                 .setCity(testData.city)
                 .submit();
+        });
 
+        step("Проверить отображение данных", () -> {
         practiceFormPage.checkResultTable("Student Name", testData.firstName + " " + testData.lastName)
                 .checkResultTable("Student Email", testData.userEmail)
                 .checkResultTable("Gender", testData.gender)
@@ -36,35 +47,48 @@ public class PracticeFormTest extends TestBase {
                 .checkResultTable("Picture", testData.picture)
                 .checkResultTable("Address", testData.currentAddress)
                 .checkResultTable("State and City", testData.state + " " + testData.city);
+        });
     }
 
     @Test
+    @DisplayName("Проверка формы с минимально заполненными данными")
     void inputMinimalData() {
-        practiceFormPage.openPage()
-                .cleanAdvertisementOnPage()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setGender(testData.gender)
-                .setPhoneNumber(testData.phoneNumber)
-                .submit();
+        step("Открыть форму", () -> {
+            practiceFormPage.openPage()
+                    .cleanAdvertisementOnPage();
+        });
 
-        practiceFormPage.checkResultTable("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResultTable("Gender", testData.gender)
-                .checkResultTable("Mobile", testData.phoneNumber);
+        step("Ввести данные", () -> {
+            practiceFormPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setGender(testData.gender)
+                    .setPhoneNumber(testData.phoneNumber)
+                    .submit();
+        });
 
+        step("Проверить отображение данных", () -> {
+            practiceFormPage.checkResultTable("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResultTable("Gender", testData.gender)
+                    .checkResultTable("Mobile", testData.phoneNumber);
+        });
     }
 
     @Test
+    @DisplayName("Проверка не валидного номера телефона")
     void inputInvalidMobileNumber() {
-        practiceFormPage.openPage()
-                .cleanAdvertisementOnPage()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setGender(testData.gender)
-                .setPhoneNumber(testData.invalidPhoneNumber)
-                .submit();
-
+        step("Открыть форму", () -> {
+            practiceFormPage.openPage()
+                    .cleanAdvertisementOnPage();
+        });
+        step("Ввести данные", () -> {
+                    practiceFormPage.setFirstName(testData.firstName)
+                            .setLastName(testData.lastName)
+                            .setGender(testData.gender)
+                            .setPhoneNumber(testData.invalidPhoneNumber)
+                            .submit();
+                });
+        step("Проверить отображение ошибки", () -> {
         practiceFormPage.verifyMobileNumberFieldIsRed();
-
-        }
+        });
+    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,10 +1,14 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-
+import org.openqa.selenium.remote.DesiredCapabilities;
 import static com.codeborne.selenide.Selenide.*;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
@@ -15,8 +19,26 @@ public class TestBase {
         Configuration.holdBrowserOpen = false;
         Configuration.timeout = 5000; // default 4000
 
-    }
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
     @AfterEach
     void clearAll() {
         clearBrowserCookies();
